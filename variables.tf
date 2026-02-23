e5c9a21e094546766c90fc3f4df99f932770dacd
srilekhayeddula54-tech <srilekhayeddula54@gmail.com>
2026-02-23 14:33:21 -0500

    initial terraform project

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "ami_value" {
+  description = "value of the ami"
+
+}
+
+variable "instance_type_value" {
+  description = "value of the instance type"
+  
+}
+
+variable "subnet_id_value" {
+    description = "value of the subnet id"
+  
+}
